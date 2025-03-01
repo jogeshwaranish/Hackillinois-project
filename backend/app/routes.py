ccd7038be12d81990fb3e8a24f5b92f3d39ec678
Anish <ani.bellamkonda@gmail.com>
2025-02-28 22:50:14 -0600

    update

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -7,6 +7,10 @@ main_bp = Blueprint("main", __name__)
 def hello():
     return jsonify({"message": "Hello from Flask!"})
 
+@main_bp.route("/location", methods=["POST"])
+def location():
+    pass
+
 # Function to register the Blueprint with the main app
 def register_routes(app):
     app.register_blueprint(main_bp)
