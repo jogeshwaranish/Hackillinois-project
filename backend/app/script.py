8f7c635a84dbb6c6557b1ada3633e405ac0affbd
Anish <ani.bellamkonda@gmail.com>
2025-03-01 00:57:55 -0600

    added some basic functions to call

--- backend/app/script.py
+++ backend/app/script.py
@@ -4,6 +4,23 @@ import os
 import requests
 import json
 
-url = f"http://api.reimaginebanking.com/enterprise/merchants?key=6ec27d1e026837afbeec1914d11ceb58"
-response = requests.get(url)
-print(response.json())
+def get_merchants_purchases():
+    url = "http://api.nessieisreal.com/merchants/67c282359683f20dd518c00b/purchases?key=6ec27d1e026837afbeec1914d11ceb58"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+def change_merchant_location(lat, lng):
+    url = "http://api.nessieisreal.com/merchants/67c282359683f20dd518c00b?key=6ec27d1e026837afbeec1914d11ceb58"
+    payload = {
+        "geocode": {
+            "lat": lat,
+            "lng": lng
+    }
+    }
+    response = requests.put(url, data=json.dumps(payload), headers={"content-type": "application/json"})
+    data = response.json()
+    return data
+
+
+
