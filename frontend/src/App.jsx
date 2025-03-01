147ea2c7de25cbff6526415d80662066c397a1a7
Basil Liu <basil.liu18@gmail.com>
2025-03-01 16:47:41 -0600

    fixed map off page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
   return (
     <>
       <Navbar />
-      <button>adghjkjhghjkhghjkldsfgfdfghgfdfghgfdfghjhgfdfghjhgfdfghjhgfdsdfghjjhvbjk</button>
       <Map />
       <Routes>
         <Route path="/" element={<Dashboard />} />
